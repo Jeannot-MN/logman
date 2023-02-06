bcf24355de9401a840f4b47321baed6c8d4d31c8
JeannotMunganga <jeannotngalula@gmail.com>
2023-02-06 15:22:40 +0200

    Removed unused imports.

--- src/main/java/com/jmn/logman/config/DataSourceConfig.java
+++ src/main/java/com/jmn/logman/config/DataSourceConfig.java
@@ -1,8 +1,8 @@
 package com.jmn.logman.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -16,8 +16,6 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import jakarta.persistence.EntityManagerFactory;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
