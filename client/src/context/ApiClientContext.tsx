d694ec206b9962614e5b6b1b325130c68c55f3fe
JeannotMunganga <jeannotngalula@gmail.com>
2023-03-13 19:05:34 +0200

    Api Client Context.

--- client/src/context/ApiClientContext.tsx
+++ client/src/context/ApiClientContext.tsx
@@ -1,5 +1,5 @@
 import {createContext} from "react";
-import axios, {Axios, AxiosRequestConfig, AxiosResponse} from "axios";
+import {Axios} from "axios";
 import {getApiServerUrl} from "../services/Utils";
 import {useAuthContext} from "./AuthContext";
 
@@ -7,20 +7,6 @@ interface Props {
     children: JSX.Element
 }
 
-interface CType{
-    get<R>(url: string): Promise<R>;
-}
-
-const t: CType = {
-    async get<R>(url: string): Promise<R> {
-        try{
-            const axiosResponse = await axios.get<R>(url);
-            return axiosResponse.data;
-        } catch (error) {
-
-        }
-    }
-}
 export const ApiClientContext = createContext<Axios>(new Axios({
     baseURL: `${getApiServerUrl()}/api/v1`
 }));
